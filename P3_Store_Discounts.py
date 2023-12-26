5e5be3a4712aab0d9a80dd6d70f193de33350571
Marco Pazol <donjulionlimon@gmail.com>
2023-12-25 21:47:01 -0600

    Adding Graphic Interface and renamed file from "P3_Discounts_OS.py" to "P3_Store_Discounts.py".

---
+++ P3_Store_Discounts.py
@@ -0,0 +1,51 @@
+#Write a program thats apply a discount to a shopping bill.
+import sqlite3
+from tkinter import ttk
+import tkinter
+import tkinter.messagebox
+
+
+"""METHODS"""
+
+#Create Database using Sqlite3
+def create_database():
+    """
+    DOCSTRING:
+        Create a database and a table if it does not exist.
+    
+    Attributes:
+        product (str): [Product]
+        price (float): [Price]
+        have_discount (boolean): [Have_Discount]
+        discount (float): [Price]
+    
+    Returns:
+        null
+    """
+    conn = sqlite3.connect('Products')
+    cursor = conn.cursor()
+    try:
+        cursor.execute('CREATE TABLE IF NOT EXIST Products(ID Integer PRIMARY KEY, Product Text NOT NULL, Price Float NOT NULL, have_discount Boolean NOT NULL, discount Float NOT NULL)')
+    except:
+        print('There was an error creating "Products" table')
+        
+#Get bill acount with applied or not applied discounts.
+def get_bill():
+    pass
+
+
+"""GRAPHIC INTERFACE"""
+
+root = tkinter.Tk()
+root.title('Store Discounts')
+root.geometry('600x400+400+100')
+root.config(bg = 'SkyBlue')
+
+root.mainloop()
+
+
+
+    
+    
+    
+    
